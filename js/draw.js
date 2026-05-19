dbd1eba26de4906598ea916355edf069a6c0e2ac
fernandoalcoba13-creator <fernandoalcoba13@gmail.com>
2026-05-19 20:17:47 -0300

    Fix printer sprite startup frame

--- js/draw.js
+++ js/draw.js
@@ -17,7 +17,7 @@ function setupPrinterAnims(scene){
 function createPrinterSprite(scene,x,y){
   if(!scene.textures.exists(PRINTER_ASSET))return null;
   setupPrinterAnims(scene);
-  const sp=scene.add.sprite(x,y+40,PRINTER_ASSET,'0.').setOrigin(.5,1).setScale(2.45).setDepth(3);
+  const sp=scene.add.sprite(x,y+40,PRINTER_ASSET,0).setOrigin(.5,1).setScale(2.45).setDepth(3);
   sp.play('printer_idle');
   return sp;
 }
